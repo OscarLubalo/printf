729d6e9a8144d193f230aab8bf88c1a4560b7991
Sandy Obare <obaresandy@gmail.com>
2023-07-28 14:24:23 +0000

    0x11. C - printf project task #0 directory clean-up

--- main.c
+++
@@ -1,8 +0,0 @@
-#include "main.h"
-int main(void)
-{
-	_printf("Character:[%c]", 'H');
-	_printf("String[%s]", "I am a string !");
-	_printf("%s%c%c%c%s%%%s%c", "Loading ", '.', '.', '.', " 99", " Please wait", '\n');
-	return (0);
-}
